4d51c159d05ac41bd82ffd39ab91615d8890f351
Chelsea Mungo <46541262+CreativeChelsea@users.noreply.github.com>
2022-05-09 16:05:45 -0400

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,31 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.util.XboxController;
 
+int FrontRight = FrontRight;
+int FrontLeft = FrontLeft;
+int BackRight = BackRight;
+int BackLeft = BackLeft;
+
+int FrontRightForward = FrontRightForward;
+int FrontLeftForward = FrontLeftForward;
+int BackRightForward = BackRightForward;
+int BackLeftForward = BackLeftForward;
+
+int FrontRightBackward = FrontRightBackward;
+int FrontLeftBackward = FrontLeftBackward;
+int BackRightBackward = BackRightBackward;
+int BackLeftBackward = BackLeftBackward;
+
+int FrontRightTurnRight= FrontRightTurnRight;
+int FrontLeftTurnRight = FrontLeftTurn ight;
+int BackRightTurnRight = BackRightTurnRight;
+int BackLeftTurnRight = BackLeftTurnRight;
+
+int FrontRightTurnLeft= FrontRightTurnLeft;
+int FrontLeftTurnLeft = FrontLeftTurnLeft;
+int BackRightTurnLeft = BackRightTurnLeft;
+int BackLeftTurnLeft = BackLeftTurnLeft;
+
 public class RobotContainer {
     private final XboxController driverController = new XboxController(0);
     private final XboxController manipulatorController = new XboxController(1);
@@ -19,3 +44,44 @@ public class RobotContainer {
         }, drivetrain));
     }
 }
+
+FrontRightForward (23, 0.9);
+FrontLeftForward (23, 0.9);
+BackLeftForward (23, 0.9);
+BackRightForward (23, 0.9);
+
+FrontRightTurnRight (23, 0.9);
+FrontLeftTurnRight(23, 0.9);
+BackLeftTurnRight(23, 0.9);
+BackRightTurnRight (23, 0.9);
+
+FrontRightForward (23, 0.9);
+FrontLeftForward (23, 0.9);
+BackLeftForward (23, 0.9);
+BackRightForward (23, 0.9);
+
+FrontRightTurnRight (23, 0.9);
+FrontLeftTurnRight(23, 0.9);
+BackLeftTurnRight(23, 0.9);
+BackRightTurnRight (23, 0.9);
+
+FrontRightForward (23, 0.9);
+FrontLeftForward (23, 0.9);
+BackLeftForward (23, 0.9);
+BackRightForward (23, 0.9);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
